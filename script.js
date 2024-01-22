9b2ea5d7fcfce274b021024571549c26295ac117
unknown <mdrohan7224@gmail.com>
2024-01-23 05:20:16 +0600

    button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let qrImage = document.querySelector(".qr-img");
 let btn = document.querySelector(".btn");
 let input = document.querySelector(".enter-text input");
+let night = document.querySelector(".toggle-btn");
+let rootBody = document.querySelector("body");
 
 function genarateQR(){
     qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`
@@ -11,3 +13,17 @@ console.log(genarateQR)
 btn.addEventListener("click", () => {
     genarateQR()
 });
+
+
+
+console.log(night);
+
+function dark () {
+    rootBody.classList.toggle("night");
+}
+
+night.addEventListener("dblclick", () => {
+    dark()
+});
+
+
